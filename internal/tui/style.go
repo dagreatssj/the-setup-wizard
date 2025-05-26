ae7f63ee0e1c52f046f068fd180bf12583e23692
Darrell Calderon <dagreatssj@gmail.com>
2025-05-26 14:50:26 -0700

    removing unused code

--- internal/tui/style.go
+++ internal/tui/style.go
@@ -10,16 +10,9 @@ const (
 )
 
 var (
-	//keywordStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("211"))
 	subtleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
-	ticksStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("79"))
 	checkboxStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212"))
-	//progressEmpty = subtleStyle.Render(progressEmptyChar)
-	dotStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("236")).Render(dotChar)
-	//mainStyle     = lipgloss.NewStyle().MarginLeft(2)
-
-	// Gradient colors we'll use for the progress bar
-	//ramp = makeRampStyles("#B14FFF", "#00FFA3", progressBarWidth)
+	dotStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("236")).Render(dotChar)
 )
 
 func checkbox(label string, checked bool) string {
